b0ada8e90d86bdbcbb27ca440603a165546f0c83
Yuquan1210 <xu1210@purdue.edu>
2023-11-02 15:35:22 -0400

    skeleton update

---
+++ IPblocklist.py
@@ -0,0 +1,8 @@
+class Analyzer(object):
+    def __init__(self):
+        self.state = 0
+        #TODO
+    
+    def todo():
+        # alternative: check whether given IPs are good or bad by querying Spamhaus 
+        return 0
--- IPclassifier.py
+++ IPclassifier.py
@@ -1,8 +1,9 @@
-class Censys(object):
+class IPclassifier(object):
     def __init__(self):
         self.state = 0
         #TODO
     
     def todo():
+        # query Censys about IP info and categorize, output dict?
         return 0
     
